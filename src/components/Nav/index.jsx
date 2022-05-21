3710678e8fec9b802fd5734764aaa5f252f6da2f
GuilhermeManfrin134 <manfrin11gui@gmail.com>
2022-05-21 17:47:12 -0300

    Adicionando Nav Mobile

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,57 +1,114 @@
-import { ImSearch } from "react-icons/im";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+//Icones
+import { ImSearch } from "react-icons/im";
+import { HiOutlineViewList } from "react-icons/hi";
+
 //Componentes de Estilização
-import { Navigator, NavPrincipal, NavItens, NavItem, NavSearch, Search } from './styles';
+import { Navigator, NavPrincipal, NavItens, NavItem, NavSearch, Search, NavigatorMobile, NavItensMobile, NavPrincipalMobile, NavigatorIconMobile } from './styles';
 
 export default function Nav(){
+
+    const [navActive, setNavActive] = useState(true);
+
+    function navMobile(){
+        setNavActive(!navActive);
+    }
+
     return(
-        <Navigator>
-            <NavPrincipal>
-                <NavItens>
+        <>
+            <Navigator>
+                <NavPrincipal>
+                    <NavItens>
+                        <NavItem>
+                            <Link to='/'>
+                                Anime
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Mangá
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Filme
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Série
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                HQ
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Podcast
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Reacts
+                            </Link>
+                        </NavItem>
+                    </NavItens>
+                    <NavSearch>
+                        <Search>
+                            <ImSearch size={20}/>
+                            <input type='text' placeholder="Ex: Anime"/>
+                        </Search>
+                    </NavSearch>
+                </NavPrincipal>
+            </Navigator>
+            <NavigatorIconMobile m={navActive ? '10px' : '210px'} >
+                <HiOutlineViewList size={35} color='#FFF' onClick={navMobile}/>
+            </NavigatorIconMobile>
+            <NavigatorMobile ml={navActive ? '-200px' : '0px'}>
+                <NavPrincipalMobile>
+                    <NavItensMobile>
                     <NavItem>
-                        <Link to='/'>
-                            Anime
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to='/'>
-                            Mangá
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to='/'>
-                            Filme
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to='/'>
-                            Série
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to='/'>
-                            HQ
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to='/'>
-                            Podcast
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to='/'>
-                            Reacts
-                        </Link>
-                    </NavItem>
-                </NavItens>
-                <NavSearch>
-                    <Search>
-                        <ImSearch size={20}/>
-                        <input type='text' placeholder="Ex: Anime"/>
-                    </Search>
-                </NavSearch>
-            </NavPrincipal>
-        </Navigator>
+                            <Link to='/'>
+                                Anime
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Mangá
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Filme
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Série
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                HQ
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Podcast
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to='/'>
+                                Reacts
+                            </Link>
+                        </NavItem>
+                    </NavItensMobile>
+                </NavPrincipalMobile>
+            </NavigatorMobile>
+        </>
     )
 }
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -7,6 +7,10 @@ export const Navigator = styled.nav`
     width: 100%;
     height: 50px;
     background-color: var(--secondaryColor);
+
+    @media screen and (max-width: 800px){
+        display: none;
+    }
 `;
 
 export const NavPrincipal = styled.section`
@@ -29,15 +33,23 @@ export const NavItem = styled.li`
     color: #FFF;
     font-weight: 800;
     transition: ease-in-out .1s;
+    -webkit-transition: ease-in-out .1s;
+    -moz-transition: ease-in-out .1s;
 
     a{
         color: #FFF;
         font-weight: 800;
         transition: ease-in-out .1s;
+        -webkit-transition: ease-in-out .1s;
+        -moz-transition: ease-in-out .1s;
     }
     a:hover{
         color: var(--clickColor);
         font-size: 20px;
+    }
+
+    @media screen and (max-width: 799px){
+        margin-bottom: 10px;
     }
 `;
 
@@ -63,4 +75,66 @@ export const Search = styled.div`
     svg{
         color: #FFF;
     }
+`;
+
+//Nav Mobile
+
+export const NavigatorIconMobile = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    background-color: var(--secondaryColor);
+    
+    svg{
+        padding-left: ${props => `${props.m}`};
+        cursor: pointer;
+        transition: all .3s;
+        -webkit-transition: all .3s;
+        -moz-transition: all .3s;
+
+        @media screen and (max-width: 350px){
+            padding-left: 10px;
+        }
+    }
+
+    @media screen and (min-width: 799px){
+        display: none;
+    }
+`;
+
+export const NavigatorMobile = styled.nav`
+    box-sizing: border-box;
+    display: flex;
+    width: 200px;
+    height: 300px;
+    background-color: var(--secondaryColor);
+    margin-top: -40px;
+    margin-left: ${props => `${props.ml}`};
+
+    transition: all .3s;
+    -webkit-transition: all .3s;
+    -moz-transition: all .3s;
+
+    position: absolute;
+    z-index: 1;
+
+    @media screen and (min-width: 799px){
+        display: none;
+    }
+    @media screen and (max-width: 350px){
+        margin-top: 0;
+    }
+`;
+
+export const NavPrincipalMobile = styled.section`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const NavItensMobile = styled.ul`
+    width: 90%;
+    height: 20px;
+    padding: 10px;
 `;
